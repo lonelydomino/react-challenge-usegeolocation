35e7010c2324f056c4f29fab9363f361ead2a883
lonelydomino <lonelydomino@icloud.com>
2025-08-24 20:37:50 -0700

    Add custom useGeolocation hook and update app structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import useGeoLocation from "./custom hooks/useGeoLocation";
 
-function App() {
+export default function App() {
+  const { isLoading, position, error, getPosition } = useGeoLocation();
+  const [countClicks, setCountClicks] = useState(0);
+
+  const { lat, lng } = position;
+  const handleClick = () => {
+    getPosition()
+    setCountClicks((c) => c + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+
+      <p>You requested position {countClicks} times</p>
     </div>
   );
 }
-
-export default App;
